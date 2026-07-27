be91c1ea262603da7b215b892d14012377acc916
Ayesha <skayesha3121@gmail.com>
2026-07-28 01:33:28 +0530

    Fixed URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
- baseURL : "https://nutrilens-backend-m6ct.onrender.com"
+ baseURL : "https://nutrilens-backend-m6ct.onrender.com/api"
 
 })
 
